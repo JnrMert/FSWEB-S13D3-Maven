9ffb9205016898112cbf166d58f9a4a9b4648247
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-09-18 12:55:01 +0300

    Person Sınıfı Solved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person person = new Person("John", "Doe", 20);
+        Person person2 = new Person("Mert", "Yazıcı", 30);
+        Person person3 = new Person("Berkay", "Emir", 24);
+
+        System.out.println("Firstname: " + person.getFirstname());
+        System.out.println("LastName: " + person.getLastname());
+        System.out.println("Age: " + person.getAge());
+
+        System.out.println("Firstname: " + person2.getFirstname());
+        System.out.println("LastName: " + person2.getLastname());
+        System.out.println("Age: " + person2.getAge());
+
+        System.out.println("Firstname: " + person3.getFirstname());
+        System.out.println("LastName: " + person3.getLastname());
+        System.out.println("Age: " + person3.getAge());
     }
 }
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Person {
+    String firstname;
+    String lastname;
+    int age;
+    boolean isTeen;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isTeen() {
+        return age >= 13 && age <= 19;
+    }
+
+    public Person(String firstname, String lastname, int age){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public Person(String firstname, String lastname, boolean isTeen, int age) {
+        this(firstname,lastname,age);
+        this.isTeen = isTeen;
+    }
+}
