85565ba1cce44c20e722d38027c15857e56aa948
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2025-06-24 15:56:23 +0300

    Check

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Person person = new Person("John", "Doe", 20);
-
+        System.out.println("-------- " + "--------------");
         System.out.println("Firstname: " + person.getFirstName());
 
         System.out.println("LastName: " + person.getLastName());
